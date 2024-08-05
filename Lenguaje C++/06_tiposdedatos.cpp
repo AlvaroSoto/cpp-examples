0bfadf5d54f45dd128ee1ed78c159998649093a6
Alvaro Soto <AlvaroSoto@users.noreply.github.com>
2024-08-04 19:53:21 -0600

    Tipos de datos

---
+++ Lenguaje C++/06_tiposdedatos.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    // Definir las variables
+    char c;
+    int i;
+    float f;
+    double d;
+    // Asignar valores a las variables
+    c = 'A';
+    i = 10;
+    f = 3.1416;
+    d = 3.14159265358979323846;
+    // Visualizar los valores de las variables
+    printf("El valor de c es: %c\n", c);
+    printf("El valor de i es: %d\n", i);
+    printf("El valor de f es: %f\n", f);
+    printf("El valor de d es: %lf\n", d);
+}
