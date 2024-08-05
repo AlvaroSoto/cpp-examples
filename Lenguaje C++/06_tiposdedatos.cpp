bf0728293a5a6b664ae3ee9296df1d14231e252c
Alvaro Soto <AlvaroSoto@users.noreply.github.com>
2024-08-04 21:30:53 -0600

    Long Double

--- Lenguaje C++/06_tiposdedatos.cpp
+++ Lenguaje C++/06_tiposdedatos.cpp
@@ -6,15 +6,18 @@ int main(void){
     int i;
     float f;
     double d;
+    long double ld;
     // Asignar valores a las variables
     c = 'A';
     i = 10;
     f = 3.1416F; // La F indica que es un número flotante
     d = 3.14159265358979323846;
+    ld = 3.14159265358979323846;
     // Visualizar los valores de las variables
     // %c: Caracter, %d: Entero, %f: Flotante, %lf: Doble
     printf("El valor de c es: %c\n", c);
     printf("El valor de i es: %d\n", i);
     printf("El valor de f es: %f\n", f);
-    printf("El valor de d es: %lf\n", d);
+    printf("El valor de d es: %.15lf\n", d); // Precisión de 15 decimales maximo
+    printf("El valor de ld es: %.18Lf\n", ld); // Precisión de 15 decimales maximo
 }
