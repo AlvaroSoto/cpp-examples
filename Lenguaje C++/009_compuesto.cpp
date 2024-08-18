87f3e23d2db29a1dc33d7d370dc7156be6165efe
Alvaro Soto <AlvaroSoto@users.noreply.github.com>
2024-08-18 16:27:37 -0600

    Numeros compuestos

--- Lenguaje C++/009_compuesto.cpp
+++ Lenguaje C++/009_compuesto.cpp
@@ -5,17 +5,10 @@ int main() {
     int num, i, j, compuesto;
     printf("Introduce un número: ");
     scanf("%d", &num);
-    printf("Los números compuestos de %d son:\n", num);
-    for (i = 2; i < num; i++) {
-        compuesto = 0;
-        for (j = 2; j < i; j++) {
-            if (i % j == 0) {
-                compuesto = 1;
-                break;
-            }
-        }
-        if (compuesto == 1) {
-            printf("%d\n", i);
+    printf("%d es número compuesto,\n", num);
+    for (i = 1; i < num; i++) {
+        if (num % i == 0) {
+            printf("porque es divisible entre: %d\n", i);
         }
     }
     return 0;
