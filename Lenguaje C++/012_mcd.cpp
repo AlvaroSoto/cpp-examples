aab085baf6babf199ca2b6779b9c3fd48ddd96b4
Alvaro Soto <AlvaroSoto@users.noreply.github.com>
2024-08-21 22:05:15 -0600

    MCD Y MCM

---
+++ Lenguaje C++/012_mcd.cpp
@@ -0,0 +1,15 @@
+// maximo común divisor (mcd) de dos números
+#include <stdio.h>
+
+int main() {
+    int num1, num2, i, mcd;
+    printf("Introduce dos números para calcular su máximo común divisor: ");
+    scanf("%d %d", &num1, &num2);
+    for (i = 1; i <= num1 && i <= num2; i++) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            mcd = i;
+        }
+    }
+    printf("El máximo común divisor de %d y %d es %d\n", num1, num2, mcd);
+    return 0;
+}
---
+++ Lenguaje C++/013_mcm.cpp
@@ -0,0 +1,16 @@
+//maximo comun multiplo (mcm) de dos numeros
+#include <stdio.h>
+
+int main() {
+    int num1, num2, i, mcm;
+    printf("Introduce dos números para calcular su mínimo común múltiplo: ");
+    scanf("%d %d", &num1, &num2);
+    for (i = 1; i <= num1 * num2; i++) {
+        if (i % num1 == 0 && i % num2 == 0) {
+            mcm = i;
+            break;
+        }
+    }
+    printf("El mínimo común múltiplo de %d y %d es %d\n", num1, num2, mcm);
+    return 0;
+}
