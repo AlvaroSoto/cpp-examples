6c5cf6d911b137438118e14378f44ab7f5668a39
Alvaro Soto <alvaro.sg.design@gmail.com>
2024-10-04 23:22:58 -0600

    Agregando el archivo de fracciones, falta analizar y modificar

---
+++ Lenguaje C++/015_fracciones.cpp
@@ -0,0 +1,35 @@
+// ! revisar código por que falta agregar "mcm" y solicitar el numerador y denominador
+// ! también solicitar si es una fracción mixta
+#include <iostream>
+
+using namespace std;
+
+struct Fraccion {
+    int numerador;
+    int denominador;
+};
+
+Fraccion sumarFracciones(Fraccion f1, Fraccion f2) {
+    int denominadorComun = f1.denominador * f2.denominador;
+    int numeradorResultante = f1.numerador * f2.denominador + f2.numerador * f1.denominador;
+    return Fraccion{numeradorResultante, denominadorComun};
+}
+
+Fraccion restarFracciones(Fraccion f1, Fraccion f2) {
+    int denominadorComun = f1.denominador * f2.denominador;
+    int numeradorResultante = f1.numerador * f2.denominador - f2.numerador * f1.denominador;
+    return Fraccion{numeradorResultante, denominadorComun};
+}
+
+int main() {
+    Fraccion f1 = {1, 2}; // 1/2
+    Fraccion f2 = {1, 3}; // 1/3
+
+    Fraccion suma = sumarFracciones(f1, f2);
+    cout << "La suma es: " << suma.numerador << "/" << suma.denominador << endl;
+
+    Fraccion resta = restarFracciones(f1, f2);
+    cout << "La resta es: " << resta.numerador << "/" << resta.denominador << endl;
+
+    return 0;
+}
