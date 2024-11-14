261a7cf5affb4ffe38ec47155d510e1143200687
Alvaro Soto <AlvaroSoto@users.noreply.github.com>
2024-11-13 22:23:01 -0600

    Modificación

--- Lenguaje C++/018_imaginarios.cpp
+++ Lenguaje C++/018_imaginarios.cpp
@@ -8,7 +8,7 @@ int main(){
     complex<double> c3;
     c3 = c1 + c2;
     printf("c3 = %.2f%+.2fi\n", c3.real(), c3.imag());
-    c3 = (1.0) / c2;
+    c3 = 1.0 / c2;
     printf("1/c2 = %.2f%+.2fi\n", c3.real(), c3.imag());
     c3 = sqrt(c1);
     printf("Raíz cuadrada de c1 = %.2f%+.2fi\n", c3.real(), c3.imag());
