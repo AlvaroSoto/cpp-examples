68929c5f0a25837ea836577fb03fda825c33069f
Alvaro Soto <AlvaroSoto@users.noreply.github.com>
2025-04-07 21:05:37 -0600

    Se realizaron 2 cambios para obtener el mismo resultado

--- Lenguaje C++/018_imaginarios.cpp
+++ Lenguaje C++/018_imaginarios.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main(){
-    complex<double> c1(1.5, 2.0);
-    complex<double> c2(-1.5, 0.5);
-    complex<double> c3;
+    complex c1(1.5, 2.0); // También se puede usar complex<double> c1(1.5, 2.0);
+    complex c2(-1.5, 0.5);
+    complex c3(0.0, 0.0); // También se puede usar complex<double> c3; Sin valores iniciales
     c3 = c1 + c2;
     printf("c3 = %.2f%+.2fi\n", c3.real(), c3.imag());
     c3 = 1.0 / c2;
